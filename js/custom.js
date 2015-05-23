785129d8ea1365af551750dbfc56b25625890d12
Devin Howard <devin@callysto.com>
2015-05-23 10:11:04 -0400

    add backbone basic stuff

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,18 @@
-var Questions = Backbone.Collection.extend({
-  url: '/raw/questions'
+$(document).ready(function() {
+  Questions = Backbone.Collection.extend({
+    url: '/raw/questions',
+  });
+  Question = Backbone.Model.extend({
+    idAttribute: "pk",
+  });
+  Answers = Backbone.Collection.extend({
+    url: '/raw/answers',
+  });
+  Answer = Backbone.Model.extend({
+    idAttribute: "pk",
+  });
+  qlst = new Questions();
+  qlst.fetch();
+  alst = new Answers();
+  alst.fetch();
 });
