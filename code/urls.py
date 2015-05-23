21d2d35e5288bc4d2eb77f71c2e9af6cb3af5095
Devin Howard <devin@callysto.com>
2015-05-23 10:00:50 -0400

    add json output for questions & answers

--- code/urls.py
+++ code/urls.py
@@ -5,5 +5,9 @@ from .views import *
 
 urlpatterns = patterns('code.views',
   url(r'^$', 'index'),
+  url(r'^raw/questions/$', 'raw_question_list', name='raw-question-list'),
+  url(r'^raw/questions/(?P<pk>\d+)/$', 'raw_question_detail', name='raw-question-detail'),
+  url(r'^raw/answers/$', 'raw_answer_list', name='raw-answer-list'),
+  url(r'^raw/answers/(?P<pk>\d+)/$', 'raw_answer_detail', name='raw-answer-detail'),
   url(r'questions/(?P<pk>\d+)/$', 'question_detail', name='question-detail'),
 )
--- code/views.py
+++ code/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
-from django.contrib.auth.decorators import login_required
-from django.views.generic import DetailView, UpdateView, ListView
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.core.urlresolvers import reverse
+
+from django.http import HttpResponse, HttpResponseBadRequest, Http404, HttpResponseForbidden
+from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
+from django.views.generic import DetailView, UpdateView, ListView
 
 from .models import *
 
 def index(request):
   return render(request, 'index.html', {})
-
-class QuestionDetailView(DetailView):
-  model = Question
-  context_object_name = 'question'
 
 def question_detail(request, pk):
   question = Question.objects.get(pk=pk)
@@ -19,3 +19,24 @@ def question_detail(request, pk):
     'question': question,
     'answer_list': answer_list,
   })
+
+def raw_question_list(request):
+  questions = Question.objects.all()
+  json_dump = serializers.serialize("json", questions)
+  return HttpResponse(json_dump, content_type="application/json")
+
+def raw_question_detail(request, pk):
+  question = get_object_or_404(Question, pk=pk)
+  json_dump = serializers.serialize("json", [question])
+  json_dump = json_dump[1:-1] #strip array bits off
+  return HttpResponse(json_dump, content_type="application/json")
+
+def raw_answer_list(request):
+  json_dump = serializers.serialize("json", Answer.objects.all())
+  return HttpResponse(json_dump, content_type="application/json")
+
+def raw_answer_detail(request, pk):
+  answer = get_object_or_404(Answer, pk=pk)
+  json_dump = serializers.serialize("json", [answer])
+  json_dump = json_dump[1:-1] #strip array bits off
+  return HttpResponse(json_dump, content_type="application/json")
