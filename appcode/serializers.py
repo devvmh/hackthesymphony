3a5b74643d5944784d225c9c9d2ed483d3b05f3c
Devin Howard <devin@callysto.com>
2015-05-23 17:19:08 -0400

    output id fields

--- appcode/serializers.py
+++ appcode/serializers.py
@@ -9,14 +9,14 @@ class QuestionSerializer(serializers.HyperlinkedModelSerializer):
 class AnswerSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Answer
-    fields = ('old_question', 'answer', 'new_question', 'redirect_url_if_no_new_question')
+    fields = ('id', 'old_question', 'answer', 'new_question', 'redirect_url_if_no_new_question')
 
 class SessionSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Session
-    fields = ('username', 'current_question')
+    fields = ('id', 'username', 'current_question')
 
 class SessionAnswerSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = SessionAnswer
-    fields = ('session', 'question', 'answer')
+    fields = ('id', 'session', 'question', 'answer')
--- appcode/urls.py
+++ appcode/urls.py
@@ -16,4 +16,5 @@ urlpatterns = patterns('appcode.views',
   url(r'^$', 'index', name='index'),
   url(r'^api/', include(router.urls)),
   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+  url(r'^js-restAPI/?$', restApi.as_view(), {'router': router, 'url_prefix':'/api,}, name='rest-api'),
 )
