aa5ab52320e10b78a4e28bac5feb02ec4a60329b
Devin Howard <devin@callysto.com>
2015-05-23 17:55:44 -0400

    move urls back it breaks stuff

--- appcode/urls.py
+++ appcode/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from django.contrib import admin
 
 from rest_framework import routers
 
@@ -15,11 +14,6 @@ router.register(r'session-answers', SessionAnswerViewSet)
 
 urlpatterns = patterns('appcode.views',
   url(r'^$', 'index', name='index'),
-
-  url(r'^login$', 'django.contrib.auth.views.login'),
-  url(r'^logout$', 'django.contrib.auth.views.logout'),
-  url(r'^admin/', include(admin.site.urls)),
-
   url(r'^api/', include(router.urls)),
   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 #  url(r'^js-restAPI/?$', restApi.as_view(), {'router': router, 'url_prefix':'/api',}, name='rest-api'),
--- djangostuff/urls.py
+++ djangostuff/urls.py
@@ -22,5 +22,8 @@ from django.views.generic import RedirectView
 from django.contrib import admin
 
 urlpatterns = patterns('',
+  url(r'^login$', 'django.contrib.auth.views.login'),
+  url(r'^logout$', 'django.contrib.auth.views.logout'),
+  url(r'^admin/', include(admin.site.urls)),
   url(r'^', include ('appcode.urls')),
 )
