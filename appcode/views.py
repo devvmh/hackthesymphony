a84fa3c32b812e9642ed5fd673e6638c763b87b5
Devin Howard <devin@callysto.com>
2015-05-23 19:11:39 -0400

    remove old code from views.py

--- appcode/views.py
+++ appcode/views.py
@@ -15,50 +15,6 @@ from .serializers import *
 def index(request):
   return render(request, 'index.html', {})
 
-def question_detail(request, pk):
-  question = Question.objects.get(pk=pk)
-  answer_list = Answer.objects.filter(old_question=question)
-  return render(request, 'question_detail.html', {
-    'question': question,
-    'answer_list': answer_list,
-  })
-
-def raw_question_list(request):
-  question_list = Question.objects.all()
-  return render(request, 'raw_question_list.html', {
-    'question_list': question_list,
-  })
-  json_dump = serializers.serialize("json", question_list)
-  return HttpResponse(json_dump, content_type="application/json")
-
-def raw_question_detail(request, pk):
-  question = get_object_or_404(Question, pk=pk)
-  return render(request, 'raw_question_detail.html', {
-    'question': question,
-  })
-  json_dump = serializers.serialize("json", [question])
-  json_dump = json_dump[1:-1] #strip array bits off
-  return HttpResponse(json_dump, content_type="application/json")
-
-def raw_answer_list(request):
-  answer_list = Answer.objects.all()
-  return render(request, 'raw_answer_list.html', {
-    'answer_list': answer_list,
-  })
-  json_dump = serializers.serialize("json", answer_list)
-  return HttpResponse(json_dump, content_type="application/json")
-
-def raw_answer_detail(request, pk):
-  answer = get_object_or_404(Answer, pk=pk)
-  return render(request, 'raw_answer_detail.html', {
-    'answer': answer,
-  })
-  json_dump = serializers.serialize("json", [answer])
-  json_dump = json_dump[1:-1] #strip array bits off
-  return HttpResponse(json_dump, content_type="application/json")
-
-
-
 class QuestionViewSet(viewsets.ModelViewSet):
     """API endpoint that allows users to be viewed or edited."""
     queryset = Question.objects.all()
