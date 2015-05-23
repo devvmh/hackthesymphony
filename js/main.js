28def421aa5fbb1802f6ec79f5c8ceec85cce983
Devin Howard <devin@callysto.com>
2015-05-23 17:56:04 -0400

    backbone collections hooked up properly now. use Model.attributes to get the row

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 function declareModelsAndCollections() {
+  Question = Backbone.Model.extend({
+  });
+  Answer = Backbone.Model.extend({
+  });
   Session = Backbone.Model.extend({
+  });
+  SessionAnswer = Backbone.Model.extend({
   });
   Questions = Backbone.Collection.extend({
     url: '/api/questions',
+    model: Question,
+    parse: function(data) {
+      return data.results;
+    },
   });
   Answers = Backbone.Collection.extend({
     url: '/api/answers',
+    model: Answer,
+    parse: function(data) {
+      return data.results;
+    },
   });
   Sessions = Backbone.Collection.extend({
     url: '/api/sessions',
-    model = Session,
+    model: Session,
+    parse: function(data) {
+      return data.results;
+    },
   });
   SessionAnswers = Backbone.Collection.extend({
     url: '/api/session-answers',
+    model: SessionAnswer,
+    parse: function(data) {
+      return data.results;
+    },
   });
-}
+}//declareModelsAndCollections
 
 function declareMainObject() {
   HDIS = new Object();
@@ -23,6 +44,7 @@ function declareMainObject() {
   HDIS.answers = new Answers();
   HDIS.answers.fetch();
   HDIS.session = new Session();
+}//declareMainObject
 
 $(document).ready(function() {
   declareModelsAndCollections();
