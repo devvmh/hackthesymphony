d4c711b74023b7733d9a8ab8a12cc4e6f58fc0fb
Devin Howard <devin@callysto.com>
2015-05-23 16:37:30 -0400

    add two more models

--- appcode/models.py
+++ appcode/models.py
@@ -3,6 +3,7 @@ from django.db.models import *
 class Question(Model):
   #question_id is not the same as pk
   question = CharField(max_length=2550)
+  #answers - foreign key from Answer class on old_question field
   def __unicode__(self): return self.question
   
 class Answer(Model):
@@ -11,3 +12,20 @@ class Answer(Model):
   new_question = ForeignKey(Question, related_name='answer_leading_here', blank=True)
   redirect_url_if_no_new_question = URLField(blank=True)
   def __unicode__(self): return self.answer + ' (' + self.old_question.question + ')'
+
+class Session(Model):
+  username = CharField(max_length=255, blank=True)
+  current_question = ForeignKey(Question, blank=True, default=Question.objects.first())
+  #answers - foreign key from QuestionsAnsweredBySession
+  def __unicode__(self):
+    if self.username:
+      return self.username
+    else:
+      return "Anonymous - on Question #" + str(self.current_question.pk)
+
+class QuestionsAnsweredBySession(Model):
+  session = ForeignKey(Session, related_name='answers')
+  question = ForeignKey(Question, related_name='sessions_answered_this_question')
+  answer = ForeignKey(Answer, related_name='sessions_using_this_answer')
+  def __unicode__(self):
+    return self.session + " answered '" + str(self.answer) + "' to '" + str(self.question) + "'."
