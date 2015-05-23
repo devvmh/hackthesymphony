473afd7c9dea3fdc111bc85f4478ebceadc870ba
Devin Howard <devin@callysto.com>
2015-05-23 11:37:25 -0400

    change name code => appcode

--- djangostuff/urls.py
+++ djangostuff/urls.py
@@ -28,5 +28,5 @@ urlpatterns = patterns('',
   url(r'^login$', 'django.contrib.auth.views.login'),
   url(r'^logout$', 'django.contrib.auth.views.logout'),
   url(r'^admin/', include(admin.site.urls)),
-  url(r'^', include ('code.urls')),
+  url(r'^', include ('appcode.urls')),
 )
