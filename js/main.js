49613e27220f4d057a47cbbbc7f70e9816071c68
Devin Howard <devin@callysto.com>
2015-05-23 20:21:59 -0400

    yay sessions are saving

--- js/main.js
+++ js/main.js
@@ -1,34 +1,78 @@
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 function declareModelsAndCollections() {
+  /* alias away the sync method */
+  Backbone._sync = Backbone.sync;
+
+  /* define a new sync method */
+  Backbone.sync = function(method, model, options) {
+      /* only need a token for non-get requests */
+      if (method == 'create' || method == 'update' || method == 'delete') {
+          var csrftoken = getCookie('csrftoken');
+
+          options.beforeSend = function(xhr){
+            var user = "admin";// your actual username
+            var pass = "password";// your actual password
+            var token = user.concat(":", pass);
+            xhr.setRequestHeader('Authorization', ("Basic ".concat(btoa(token))));
+            xhr.setRequestHeader('X-CSRFToken', csrftoken);
+          };
+      }//if
+
+      /* proxy the call to the old sync method */
+      return Backbone._sync(method, model, options);
+  };
+  BaseDjangoCollection = Backbone.Collection.extend({
+    parse: function(data) {
+      return data.results;
+    },
+  });
   Question = Backbone.Model.extend({
   });
   Answer = Backbone.Model.extend({
   });
   Session = Backbone.Model.extend({
+    urlRoot: '/api/sessions',
   });
   SessionAnswer = Backbone.Model.extend({
   });
-  Questions = Backbone.Collection.extend({
+  Questions = BaseDjangoCollection.extend({
     url: '/api/questions',
     model: Question,
     parse: function(data) {
       return data.results;
     },
   });
-  Answers = Backbone.Collection.extend({
+  Answers = BaseDjangoCollection.extend({
     url: '/api/answers',
     model: Answer,
     parse: function(data) {
       return data.results;
     },
   });
-  Sessions = Backbone.Collection.extend({
+  Sessions = BaseDjangoCollection.extend({
     url: '/api/sessions',
     model: Session,
     parse: function(data) {
       return data.results;
     },
   });
-  SessionAnswers = Backbone.Collection.extend({
+  SessionAnswers = BaseDjangoCollection.extend({
     url: '/api/session-answers',
     model: SessionAnswer,
     parse: function(data) {
@@ -38,12 +82,17 @@ function declareModelsAndCollections() {
 }//declareModelsAndCollections
 
 function declareMainObject() {
-  ORCA = new Object();
   ORCA.questions = new Questions();
   ORCA.questions.fetch();
   ORCA.answers = new Answers();
   ORCA.answers.fetch();
-  ORCA.session = new Session();
+  ORCA.first_question_url = window.location.href + 'api/questions/1';
+  ORCA.session = new Session({
+    username: "Anonymous",
+    ip: ORCA.ip_address,
+    current_question: ORCA.first_question_url,
+  });
+  ORCA.session.save();
 }//declareMainObject
 
 $(document).ready(function() {
