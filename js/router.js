1f86ad1de0fa916c0569e8bfdb5b229399a5159b
Devin Howard <devin@callysto.com>
2015-05-23 23:11:39 -0400

    back button works

--- js/router.js
+++ js/router.js
@@ -1,11 +1,24 @@
-
 ORCARouter = Backbone.Router.extend({
+  initialize: function(options) {
+    this.history = [];
+    this.listenTo(this, 'route', function(name, args) {
+      this.history.push({
+        name : name,
+        args : args,
+        fragment : Backbone.history.fragment
+      });
+    });
+  },
   routes: {
     "questions/:id": "renderQuestion",
   },
   renderQuestion(id) {
+    console.log("got here 2");
+    //render question text
     q = ORCA.questions.get(id);
     $('.question p').html(q.attributes.question);
+    
+    //render answers
     $('.answers').html('');
     $.each(q.answers(), function(index, value) {
       var answer = value.attributes.answer;
@@ -19,6 +32,17 @@ ORCARouter = Backbone.Router.extend({
       $('.answer-' + id + ' a').click(function() {
         ORCA.router.navigate(new_question, {trigger: true});
       });
+    });
+
+    //set up back button
+    $('#back-button a').unbind('click');
+    $('#back-button a').click(function() {
+      console.log(ORCA.router.history);
+      //pop current page, then pop previous page to use;
+      //navigate will add the previous page back on
+      ORCA.router.history.pop();
+      ORCA.router.navigate(ORCA.router.history.pop().fragment, {trigger: true});
+      console.log(ORCA.router.history);
     });
   },
 });
