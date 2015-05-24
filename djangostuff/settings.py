d901c8c06927354e22f9358e3dd3d0a98a248124
Devin Howard <devin@callysto.com>
2015-05-23 22:44:34 -0400

    images folder

--- djangostuff/settings.py
+++ djangostuff/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, "assets"),
+  os.path.join(BASE_DIR, "images"),
   os.path.join(BASE_DIR, "css"),
   os.path.join(BASE_DIR, "js"),
 )
