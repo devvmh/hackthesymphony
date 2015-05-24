4905eb9ff189afd3e0e0354f0c0afb7bd2a33d87
Devin Howard <devin@callysto.com>
2015-05-24 00:14:36 -0400

    csv to json script yay

---
+++ csv_to_json.py
@@ -0,0 +1,31 @@
+import csv, sys, json
+
+fixtures = []
+
+with open(sys.argv[1], 'rb') as csvfile:
+  csvreader = csv.reader(csvfile, delimiter = ',', quotechar='"')
+  for row in csvreader:
+    obj = {}
+    obj['model'] = "appcode.questions"
+    obj['pk'] = row[1]
+    obj['fields'] = {}
+    obj['fields']['question'] = row[0]
+    fixtures.append(obj)
+pk = 1
+with open(sys.argv[2], 'rb') as csvfile:
+  csvreader = csv.reader(csvfile, delimiter = ',', quotechar='"')
+  for row in csvreader:
+    obj = {}
+    obj['model'] = "appcode.questions"
+    obj['pk'] = pk
+    pk += 1
+    obj['fields'] = {}
+    obj['fields']['answer'] = row[0]
+    obj['fields']['old_question'] = row[1]
+    if row[2]:
+      obj['fields']['new_question'] = row[2]
+    if row[3]:
+      obj['fields']['comment'] = row[3]
+    fixtures.append(obj)
+
+print json.dumps(fixtures, indent=2)
