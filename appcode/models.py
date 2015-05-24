49a2f024f7cd0321a8d0a3011acc590fab9d6059
Devin Howard <devin@callysto.com>
2015-05-23 21:30:28 -0400

    add comment field to answers

--- appcode/models.py
+++ appcode/models.py
@@ -9,6 +9,7 @@ class Question(Model):
 class Answer(Model):
   old_question = ForeignKey(Question, related_name='answers')
   answer = CharField(max_length=2550)
+  comment = CharField(max_length=2550, blank=True)
   new_question = ForeignKey(Question, related_name='answer_leading_here', blank=True)
   redirect_url_if_no_new_question = URLField(blank=True)
   def __unicode__(self): return self.answer + ' (' + self.old_question.question + ')'
