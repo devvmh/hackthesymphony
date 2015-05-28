8a023e6e63240492487a465ba6efd44f46122fb6
Devin Howard <devin@callysto.com>
2015-05-27 21:52:41 -0400

    remove js-restAPI nonsense

--- appcode/urls.py
+++ appcode/urls.py
@@ -18,5 +18,4 @@ urlpatterns = patterns('appcode.views',
   url(r'suggestions/(?P<pk>\d+)/$', 'suggestions', name='suggestions'),
   url(r'^api/', include(router.urls)),
   url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-#  url(r'^js-restAPI/?$', restApi.as_view(), {'router': router, 'url_prefix':'/api',}, name='rest-api'),
 )
