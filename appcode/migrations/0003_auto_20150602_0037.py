dbd1f0da7b08a8fcb0115d09163748e39845efb5
Devin Howard <devin@callysto.com>
2015-06-01 20:37:56 -0400

    add protip field to models

---
+++ appcode/migrations/0003_auto_20150602_0037.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('appcode', '0002_auto_20150602_0029'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='answer',
+            name='protip',
+            field=models.CharField(max_length=2550, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='answer',
+            name='comment',
+            field=models.CharField(max_length=2550, null=True, blank=True),
+        ),
+    ]
