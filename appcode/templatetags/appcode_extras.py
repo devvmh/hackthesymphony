da69f899e7ad6d57214d989e111c6e59cc6614f1
Devin Howard <devin@callysto.com>
2015-06-05 00:00:05 -0400

    use http_referer to populate what-is-this back buttons

--- appcode/templatetags/appcode_extras.py
+++ appcode/templatetags/appcode_extras.py
@@ -5,6 +5,3 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(str(key))
-
-def get_score_from_concert_answer(dictionary, cindex, aindex):
-  pass
