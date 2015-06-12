17b9c0177e5bb6b72f183bf71f392b6de11e6548
Devin Howard <devin@callysto.com>
2015-06-11 20:11:07 -0400

    store session answers a bit differently

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
       ip: ORCA.ip_address,
       current_question: window.location.origin + '/api/questions/1',
       session_token: Math.random().toString(36).slice(2),
+      session_answers: [],
     });
     ORCA.router.navigate('questions/1', {trigger: true});
     localStorage.setItem('session', JSON.stringify(ORCA.session));
